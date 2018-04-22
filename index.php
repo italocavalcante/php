2cac04becef95435c9f8e7f226c9d80673f2d415
italocavalcante <italocavalcantefortaleza@live.com>
2018-04-22 18:14:14 -0300

    Novas alterações

--- index.php
+++ index.php
@@ -1,9 +1,5 @@
 <?php
 	echo "Olá mundo";
-<<<<<<< HEAD
-	echo "Portuscale consulting123";
-=======
 	echo "Portuscale consulting";
 	echo "Portuscaleconsulting.com.br";
->>>>>>> 7b51f5eb5d52e19a6283fcc490e68aab1f713698
 ?>
